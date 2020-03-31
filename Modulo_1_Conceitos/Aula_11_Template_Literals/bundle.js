2b1d530146a1885f82d70653a98c6d25e98c54a0
KevsonDoc <filipemorais576@gmail.com>
2020-03-31 17:25:05 -0300

    [ADD] - Aula 11 Template Literals

---
+++ Modulo_1_Conceitos/Aula_11_Template_Literals/bundle.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var nome = "Diego";
+var idade = 23;
+console.log("eu nome \xE9 ".concat(nome, " e tenho ").concat(idade, " anos"));
---
+++ Modulo_1_Conceitos/Aula_11_Template_Literals/main.js
@@ -0,0 +1,4 @@
+const nome = "Diego";
+const idade = 23;
+
+console.log(`eu nome é ${nome} e tenho ${idade} anos`);
