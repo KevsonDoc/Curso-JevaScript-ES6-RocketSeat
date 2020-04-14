fc26d8aad401e57fcf2f7a8bf4d1042fec08293c
KevsonDoc <filipemorais576@gmail.com>
2020-04-13 21:24:00 -0300

    [ADD] - Exercicos modulo 2 completo

---
+++ Modulo_2_WebPack_Server/Desafios/Exercicio_1/src/main.js
@@ -0,0 +1,10 @@
+//1.1
+import ClassUsiario from './function';
+//import { idade } from './function';
+
+//1.2
+ClassUsiario.info();
+alert(idade);
+
+//1.3
+import defaultUsuario, { idade as idadeUsuario }  from './function';
