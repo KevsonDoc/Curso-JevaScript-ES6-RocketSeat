a621b3739c2779f2bc3678d0e7d5776b8e593954
KevsonDoc <filipemorais576@gmail.com>
2020-04-21 23:38:23 -0300

    [ADD] - Modulo 4 Aula 4

--- Modulo_3_Async-Await/Aula_2-Configurando axios/src/main.js
+++ Modulo_3_Async-Await/Aula_2-Configurando axios/src/main.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class Api {
     static async getUserInfo(username) {
         try {
-        const response = await axios.get(`https://api.github.com/users/${username}`);
+            const response = await axios.get(`https://api.github.com/users/${username}`);
             console.log(response);
         } catch {
             console.warn('Erro na API');
