07f27d92d73cda2964e8539d9ba07b0df42a2c2c
KevsonDoc <filipemorais576@gmail.com>
2020-04-21 22:22:38 -0300

    [ADD] - Modulo 4 Aula 4

--- Modulo_4_Aplicação_Com_ES6+/Aula_1_2_3_4_5/src/main.js
+++ Modulo_4_Aplicação_Com_ES6+/Aula_1_2_3_4_5/src/main.js
@@ -1,10 +1,12 @@
+import api from './api';
+
 class App {
     constructor() {
         this.repositories = [];
 
         this.formEl = document.getElementById('repo-form');
         this.listEl = document.getElementById('repo-lis');
-
+        this.inputEl = document.querySelector('input[name=repository]');
         this.registerHandlers();
     }
     
@@ -13,14 +15,22 @@ class App {
         this.formEl.onsubmit = envent => this.addRepository(envent);
     }
 
-    addRepository(event){
+    async addRepository(event){
         event.preventDefault();
 
+        const repoInput = this.inputEl.value;
+        if (repoInput.length === 0)
+            return;
+
+        const response = await api.get(`/repos/${repoInput}`);
+
+        const { name, description, html_url, owner: { avatar_url } } = response.data;
+
         this.repositories.push({
-            name: 'rocketseat.com.br',
-            description: 'Tire a sua ideia do papel e dê vida à sua startup.',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-            html_url: 'https://github.com/Rocketseat/gostack-template-fundamentos-reactjs',
+            name,
+            description,
+            avatar_url,
+            html_url,
         });
 
         this.render();
@@ -41,6 +51,7 @@ class App {
 
             let linkEl = document.createElement('a');
             linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', repo.html_url);
             linkEl.appendChild(document.createTextNode('Acessar'));
 
             let listItemEl = document.createElement('li');
